07036ba9c633ecbd8866a9c20aa6b76fafeeff79
sticklercarla <carlastickler@gmail.com>
2019-08-14 18:42:59 -0400

    fixed time...sort of

--- src/Components/Audition.js
+++ src/Components/Audition.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import UpdateAudition from './UpdateAudition'
+import Moment from 'react-moment';
 
 class Audition extends React.Component {
 
@@ -9,6 +10,22 @@ class Audition extends React.Component {
     
     handleClick = () => {
         this.setState({ cardFlipped: !this.state.cardFlipped })
+    }
+
+    handleDate = () => {
+        const time = this.props.audition.time.split("T")
+        const date = this.props.audition.date
+        const date_time = (date + "T" + time[1])
+        const dateToFormat = date_time
+        return <Moment format="MM/DD/YYYY">{dateToFormat}</Moment>
+    }
+
+    handleTime = () => {
+        const time = this.props.audition.time.split("T")
+        const date = this.props.audition.date
+        const date_time = (date + "T" + time[1])
+        const new_date = new Date(date_time);
+        return <span>{new_date.getUTCHours()}:{new_date.getUTCMinutes()} {(parseInt(time[1].split(":")[0])) > 12 ? "PM" : "AM"}</span>
     }
 
     render() {
@@ -21,8 +38,10 @@ class Audition extends React.Component {
                 :
                 (<div className="card-info">
                     <h1>{this.props.audition.musical_title}</h1>
-                    <p>Date: {this.props.audition.date}</p>
-                    <p>Time: {this.props.audition.time}</p>
+                    <div>
+                        <span>Date: {this.handleDate()} </span>
+                        <span>Time: {this.handleTime()}</span>
+                    </div>
                     <p>Style: {this.props.audition.style}</p>
                     <p>Song Length: {this.props.audition.song_length}</p>
                     <p>{this.props.audition.appointment ? "Appointment" : "Open-Call"}</p>
